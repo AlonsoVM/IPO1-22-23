e9d6f13978f6c7c11051e1635f68fd003d19830c
AlonsoVM <Alonso.Villamayor@alu.uclm.es>
2022-12-26 19:00:49 +0100

    cambios en la interfaz principla

--- InterfazPrincipal.xaml.cs
+++ InterfazPrincipal.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace Senderismo
 {
@@ -19,9 +21,52 @@ namespace Senderismo
     /// </summary>
     public partial class InterfazPrincipal : Window
     {
+        private List<Senderistas> listaSenderistas;
         public InterfazPrincipal()
         {
             InitializeComponent();
+            listaSenderistas = CargarContenidoXML_S();
+            lstSenderistas.ItemsSource = listaSenderistas;
+        }
+
+        private List<Senderistas> CargarContenidoXML_S()
+        {
+            List<Senderistas> aux = new List<Senderistas>();
+            XmlDocument doc2 = new XmlDocument();
+            var fichero2 = Application.GetResourceStream(new Uri("data/Senderistas.xml", UriKind.Relative));
+            doc2.Load(fichero2.Stream);
+
+            foreach (XmlNode node in doc2.DocumentElement.ChildNodes)
+            {
+                var nuevoSocio = new Senderistas("", "", "", "", "", "", "", null);
+
+                nuevoSocio.nombreS = node.Attributes["nombreS"].Value;
+                nuevoSocio.dni = node.Attributes["dni"].Value;
+                nuevoSocio.telefono = (node.Attributes["telefono"].Value);
+                nuevoSocio.correo = node.Attributes["correo"].Value;
+                nuevoSocio.cuentaBancaria = node.Attributes["cuentaBancaria"].Value;
+                nuevoSocio.cuantiaAyuda = (node.Attributes["cuantiaAyuda"].Value);
+                nuevoSocio.formaPago = node.Attributes["formaPago"].Value;
+                nuevoSocio.foto_S = new Uri(node.Attributes["foto_S"].Value, UriKind.Relative);
+                aux.Add(nuevoSocio);
+            }
+
+            return aux;
+        }
+
+        private void annadir_senderias_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Eliminar_senderista_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lstSenderistas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Senderistas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senderismo
+{
+    class Senderistas
+    {
+        public string nombreS { set; get; }
+        public string dni { set; get; }
+        public string telefono { set; get; }
+        public string correo { set; get; }
+        public string cuentaBancaria { set; get; }
+        public string cuantiaAyuda { set; get; }
+        public string formaPago { set; get; }
+        public Uri foto_S { set; get; }
+
+        public Senderistas(string nombreS, string dni, string telefono, string correo, string cuentaBancaria, string cuantiaAyuda, string formaPago, Uri foto_S)
+        {
+            this.nombreS = nombreS;
+            this.dni = dni;
+            this.telefono = telefono;
+            this.correo = correo;
+            this.cuentaBancaria = cuentaBancaria;
+            this.cuantiaAyuda = cuantiaAyuda;
+            this.formaPago = formaPago;
+            this.foto_S = foto_S;
+        }
+    }
+}
