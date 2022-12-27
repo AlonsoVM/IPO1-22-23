53f892e7314da40a2890ce6982decd9c214ebc0d
AlonsoVM <Alonso.Villamayor@alu.uclm.es>
2022-12-27 12:35:55 +0100

    avances IU_gestion_excursionistas

--- InterfazPrincipal.xaml.cs
+++ InterfazPrincipal.xaml.cs
@@ -38,17 +38,21 @@ namespace Senderismo
 
             foreach (XmlNode node in doc2.DocumentElement.ChildNodes)
             {
-                var nuevoSocio = new Senderistas("", "", "", "", "", "", "", null);
+                var senderista_aux = new Senderistas("", "", "", "", "", "", "", null, "", "", "", "");
 
-                nuevoSocio.nombreS = node.Attributes["nombreS"].Value;
-                nuevoSocio.dni = node.Attributes["dni"].Value;
-                nuevoSocio.telefono = (node.Attributes["telefono"].Value);
-                nuevoSocio.correo = node.Attributes["correo"].Value;
-                nuevoSocio.cuentaBancaria = node.Attributes["cuentaBancaria"].Value;
-                nuevoSocio.cuantiaAyuda = (node.Attributes["cuantiaAyuda"].Value);
-                nuevoSocio.formaPago = node.Attributes["formaPago"].Value;
-                nuevoSocio.foto_S = new Uri(node.Attributes["foto_S"].Value, UriKind.Relative);
-                aux.Add(nuevoSocio);
+                senderista_aux.nombreS = node.Attributes["nombreS"].Value;
+                senderista_aux.dni = node.Attributes["dni"].Value;
+                senderista_aux.telefono = (node.Attributes["telefono"].Value);
+                senderista_aux.telefono_m = (node.Attributes["telefono_m"].Value);
+                senderista_aux.correo = node.Attributes["correo"].Value;
+                senderista_aux.apellido = node.Attributes["apellidos"].Value;
+                senderista_aux.rutas_realizadas = node.Attributes["rutas_realizadas"].Value;
+                senderista_aux.foto_S = new Uri(node.Attributes["foto_S"].Value, UriKind.Relative);
+                senderista_aux.edad = node.Attributes["edad"].Value;
+                senderista_aux.sexo = node.Attributes["sexo"].Value;
+                senderista_aux.direccion = node.Attributes["direccion"].Value;
+                senderista_aux.participacion = (node.Attributes["participacion"].Value);
+                aux.Add(senderista_aux);
             }
 
             return aux;
@@ -56,12 +60,18 @@ namespace Senderismo
 
         private void annadir_senderias_Click(object sender, RoutedEventArgs e)
         {
-
+            Senderistas senderista = new Senderistas("Nuevo Elemento", "", "", "", "", "", "", new Uri("fotos/jose.jpg", UriKind.Relative), "", "", "", "");
+            btnGuardar.Visibility = Visibility.Visible;
+            listaSenderistas.Add(senderista);
+            lstSenderistas.Items.Refresh();
+            MessageBox.Show("Ya se pude introducir información del nuevo senderitsta");
         }
 
         private void Eliminar_senderista_click(object sender, RoutedEventArgs e)
         {
-
+            listaSenderistas.RemoveAt(lstSenderistas.SelectedIndex);
+            lstSenderistas.Items.Refresh();
+            MessageBox.Show("se elimino al senderista");
         }
 
         private void lstSenderistas_SelectionChanged(object sender, SelectionChangedEventArgs e)
--- Senderistas.cs
+++ Senderistas.cs
@@ -12,21 +12,34 @@ namespace Senderismo
         public string dni { set; get; }
         public string telefono { set; get; }
         public string correo { set; get; }
-        public string cuentaBancaria { set; get; }
-        public string cuantiaAyuda { set; get; }
-        public string formaPago { set; get; }
+        public string apellido { set; get; }
+        public string rutas_realizadas { set; get; }
         public Uri foto_S { set; get; }
 
-        public Senderistas(string nombreS, string dni, string telefono, string correo, string cuentaBancaria, string cuantiaAyuda, string formaPago, Uri foto_S)
+        public string edad { set; get; }
+
+        public string sexo { set; get; }
+
+        public string direccion { set; get; }
+
+        public string telefono_m { set; get; }
+
+        public string participacion { set; get; }
+
+        public Senderistas(string nombreS, string dni, string telefono, string telefono_m, string correo, string apellido, string rutas_realizadas, Uri foto_S, string edad, string Sexo, string dir, string participacion)
         {
             this.nombreS = nombreS;
             this.dni = dni;
             this.telefono = telefono;
+            this.telefono_m = telefono_m;
             this.correo = correo;
-            this.cuentaBancaria = cuentaBancaria;
-            this.cuantiaAyuda = cuantiaAyuda;
-            this.formaPago = formaPago;
+            this.apellido = apellido;
+            this.rutas_realizadas = rutas_realizadas;
             this.foto_S = foto_S;
+            this.edad = edad;
+            this.sexo = sexo;
+            this.direccion = dir;
+            this.participacion = participacion;
         }
     }
 }
